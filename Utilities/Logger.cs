8fbd54dae4c6b88cd080700041fe0545c78c27a9
Morteza Hosseini <morteza_hosseini_dev@outlook.com>
2024-11-21 16:32:34 +0330

    Logger Create Logger class with Log function to save program logs into file

---
+++ Utilities/Logger.cs
@@ -0,0 +1,21 @@
+﻿namespace ResourceBroker.Utilities
+{
+    public class Logger
+    {
+        private const string LogFilePath = "mainlogs.txt";
+
+        public static async Task Log(string message)
+        {
+            try
+            {
+                var logEntry = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {message}";
+
+                await File.AppendAllTextAsync(LogFilePath, logEntry + Environment.NewLine);
+            }
+            catch (Exception)
+            {
+                return;
+            }
+        }
+    }
+}
